1fa1266859b0ae7dd241d5c0d0b6eaabd0980d68
Sahana8055 <sahananarayan11@gmail.com>
2024-05-07 12:06:12 +0530

    inheritance

--- demo2.java
+++ demo2.java
@@ -1,11 +1,12 @@
 import java.util.Scanner;
-class A
+
+class Dev
 {
 int id,age;
 String name;
-float salary;
-String designation;
-  A()
+float salary=50000;
+String designation="dev";
+Dev()
 {
 Scanner sc=new Scanner(System.in);
 System.out.println("enter name ");
@@ -24,20 +25,55 @@ System.out.println("designation ");
 designation=sc.next();
 }
 void display(){
-System.out.println("id" +id);
-System.out.println("name" +name);
-System.out.println("age" +age);
-System.out.println("salary" +salary);
-System.out.println("designation" +designation);
+System.out.println("id: " +id);
+System.out.println("name: " +name);
+System.out.println("age: " +age);
+System.out.println("salary: " +salary);
+System.out.println("designation : " +designation);
+}
 }
 
+class Clerk
+{
+int id,age;
+String name;
+float salary=50000;
+String designation="dev";
+Clerk()
+{
+Scanner sc=new Scanner(System.in);
+System.out.println("enter name ");
+name=sc.next();
+
+System.out.println("enter id ");
+id=sc.nextInt();
+
+System.out.println("age ");
+age=sc.nextInt();
+
+System.out.println("salary ");
+salary=sc.nextFloat();
+
+System.out.println("designation ");
+designation=sc.next();
+}
+void display(){
+System.out.println("id: " +id);
+System.out.println("name: " +name);
+System.out.println("age: " +age);
+System.out.println("salary: " +salary);
+System.out.println("designation : " +designation);
 }
 public class demo2
 {
 public static void main(String args[]){
   System.out.println("hi njava");
-  A a=new A();
-  a.display();
+  Dev d=new Dev();
+  d.display();
+  Clerk c=new Clerk();
+  c.display();
+
+}
 }
 }
 
---
+++ demo3.java
@@ -0,0 +1,11 @@
+Class P{
+    {
+        int money=20000;
+        String Car="bmw";
+        private String newCar="BENZ";
+        int site= 5;
+        void use(){
+        sout
+        }
+    }
+}
